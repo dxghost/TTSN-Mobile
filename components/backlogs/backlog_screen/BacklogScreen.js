9c7916bf927a38206a2626d118013f10fd5e4050
Mahdi Pazoki <mahdipazooki21@gmail.com>
2019-05-28 13:26:26 +0430

    add sortablity feature + drag icon

--- components/backlogs/backlog_screen/BacklogScreen.js
+++ components/backlogs/backlog_screen/BacklogScreen.js
@@ -1,28 +1,161 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
+import {
+    Animated,
+    Easing,
+    StyleSheet,
+    Text,
+    Image,
+    View,
+    Dimensions,
+    Platform,
+} from 'react-native';
 import { Avatar, Divider, ListItem, Header, Icon } from "react-native-elements";
+import SortableList from 'react-native-sortable-list';
 
-const list = [
-]
+const window = Dimensions.get('window');
+const data = {
+    0: {
+        image: 'https://placekitten.com/200/240',
+        text: 'Chloe',
+    },
+    1: {
+        image: 'https://placekitten.com/200/201',
+        text: 'Jasper',
+    },
+    2: {
+        image: 'https://placekitten.com/200/202',
+        text: 'Pepper',
+    },
+    3: {
+        image: 'https://placekitten.com/200/203',
+        text: 'Oscar',
+    },
+    4: {
+        image: 'https://placekitten.com/200/204',
+        text: 'Dusty',
+    },
+    5: {
+        image: 'https://placekitten.com/200/205',
+        text: 'Spooky',
+    },
+    6: {
+        image: 'https://placekitten.com/200/210',
+        text: 'Kiki',
+    },
+    7: {
+        image: 'https://placekitten.com/200/215',
+        text: 'Smokey',
+    },
+    8: {
+        image: 'https://placekitten.com/200/220',
+        text: 'Gizmo',
+    },
+    9: {
+        image: 'https://placekitten.com/220/239',
+        text: 'Kitty',
+    },
+};
 
 
 export default class BacklogScreen extends React.Component {
-    keyExtractor = (item, index) => index.toString()
-    renderItem = ({ item }) => (
-        <ListItem
-        />
-    )
     render() {
         return (
-            <View>
-
+            <View style={styles.container}>
+                <Text style={styles.title}>React Native Sortable List</Text>
+                <SortableList
+                    style={styles.list}
+                    contentContainerStyle={styles.contentContainer}
+                    data={data}
+                    renderRow={this._renderRow} />
             </View>
         );
     }
+
+    _renderRow = ({ data, active }) => {
+        return <Row data={data} active={active} />
+    }
 }
-PeopleList.navigationOptions = {
+
+
+class Row extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this._active = new Animated.Value(0);
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.props.active !== nextProps.active) {
+            Animated.timing(this._active, {
+                duration: 300,
+                easing: Easing.bounce,
+                toValue: Number(nextProps.active),
+            }).start();
+        }
+    }
+
+    render() {
+        const { data, active } = this.props;
+
+        return (
+            <ListItem
+                style={styles.row}
+                leftIcon={
+                    <Icon
+                        name='drag'
+                        type='material-community'
+                        color='#E0005A' />
+
+                }
+                // key={item.index}
+                // leftAvatar={{ source: { uri: item.avatar_url } }}
+                title={data.text}
+            // subtitle={item.subtitle}
+            />
+        );
+    }
+}
+
+BacklogScreen.navigationOptions = {
     drawerLabel: 'Backlogs',
     // drawerIcon: () => (
     //     <Image source={PeopleIcon} />
     // ),
 };
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#eee',
+    },
+
+    title: {
+        fontSize: 20,
+        paddingVertical: 20,
+        marginTop: "3%",
+        color: '#999999',
+    },
+
+    list: {
+        flex: 1,
+    },
+
+    contentContainer: {
+        width: window.width,
+    },
+
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        height:60,
+        flex: 1,
+        marginTop: 2,
+        marginBottom: 2,
+    },
+});
