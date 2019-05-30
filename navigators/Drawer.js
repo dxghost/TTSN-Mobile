d20d723f871f5033f0358f8296041f3cf4560875
Mahdi Pazoki <mahdipazooki21@gmail.com>
2019-05-30 20:59:02 +0430

    delete samplePeople component

--- navigators/Drawer.js
+++ navigators/Drawer.js
@@ -1,6 +1,5 @@
 import { createDrawerNavigator } from 'react-navigation';
 import PeopleScreen from '../components/people/people_list/PeopleList';
-import SamplePeople from '../components/people/people_list/samplePeople';
 import BacklogScreen from '../screens/BacklogScreen'
 import BacklogDetailScreen from '../screens/BacklogDetail'
 
@@ -8,9 +7,6 @@ const Drawer = createDrawerNavigator(
   {
     Drawer1: {
       screen: PeopleScreen,
-    },
-    Drawer2: {
-      screen: SamplePeople,
     },
     Drawer3: {
       screen: BacklogScreen,
