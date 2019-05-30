9b40bcd2afdfd790bd09953d391c453047743b2d
Mahdi Pazoki <mahdipazooki21@gmail.com>
2019-05-30 20:16:25 +0430

    init backlog details

--- navigators/Drawer.js
+++ navigators/Drawer.js
@@ -2,6 +2,7 @@ import { createDrawerNavigator } from 'react-navigation';
 import PeopleScreen from '../components/people/people_list/PeopleList';
 import SamplePeople from '../components/people/people_list/samplePeople';
 import BacklogScreen from '../screens/BacklogScreen'
+import BacklogDetailScreen from '../screens/BacklogDetail'
 
 const Drawer = createDrawerNavigator(
   {
@@ -13,10 +14,13 @@ const Drawer = createDrawerNavigator(
     },
     Drawer3: {
       screen: BacklogScreen,
+    },
+    Drawer4:{
+      screen:BacklogDetailScreen,
     }
   },
   {
-    initialRouteName: 'Drawer3',
+    initialRouteName: 'Drawer4',
     drawerPosition: 'left',
     contentOptions: {
       activeTintColor: '#e91e63',
---
+++ screens/BacklogDetail.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {View,Text } from 'react-native'
+
+export default class BacklogDetailScreen extends React.Component {
+    render() {
+        return (
+            <View>
+                
+            </View>
+        )
+    }
+}
+BacklogDetailScreen.navigationOptions = {
+    drawerLabel: 'Backlog Details',
+    // drawerIcon: () => (
+    //     <Image source={PeopleIcon} />
+    // ),
+};
