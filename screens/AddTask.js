2676511b524d3f1077932c97431267838d92714f
mohammadkhavari <mse737.khavari@gmail.com>
2019-06-07 15:44:33 +0430

    Fetch backLogs from backend and embed them in DropDown to select a bacllog

--- screens/AddTask.js
+++ screens/AddTask.js
@@ -6,16 +6,42 @@ import {Button} from 'react-native-elements'
 
 
 export default class AddTask extends React.Component{
-    state = {taskDsr:'', taskName:'', backLog:''};
+
+    state = {taskDsr:'', 
+            taskName:'', 
+            backlogId:'', 
+            fetchingBacklogs:true, 
+            backlogData: [],
+            selectedBackLog : ''
+        };
+
+    requestHandler = async () => {
+        let apiUrl = 'http://mamaly100.pythonanywhere.com/Backlog/';
+        let formData = new FormData();
+        let options = {
+            method: 'GET',
+            // body: formData,
+            headers: {
+                Accept: '*/*',
+                'Content-Type': 'application/json',
+                // 'Authorization': 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvcmlnX2lhdCI6MTU1OTU1NjUxNCwiZXhwIjoxNTU5NTYyNTE0LCJ1c2VyX2lkIjoxLCJlbWFpbCI6Im1haGRpcGF6b29raTIxQGdtYWlsLmNvbSIsInVzZXJuYW1lIjoiZHgifQ.kCdXNmh_o28eLCPsHOwIMefYE12ckg2QI0uMkfIsWZw'
+            }
+        };
+        return fetch(apiUrl, options)
+    }
+
+    componentWillMount = async () => {
+        var f = await this.requestHandler()
+        f = await f.json()
+        //console.log(f)
+        this.setState({
+            backlogData: f,
+            fetchingBacklogs: false
+        })
+    }
+    
     render(){
-        let data = [{
-            value: 'updateDB',
-          }, {
-            value: 'AddName',
-          }, {
-            value: 'BuildMain',
-          }];
-        let {taskDsr, taskName, backLog} = this.state;
+        let {taskDsr, taskName,} = this.state;
         return(
             <View style={styles.container}>
                 <View>
@@ -26,25 +52,28 @@ export default class AddTask extends React.Component{
                 onChangeText={(taskName) => this.setState({taskName})}/>
 
                 <TextField
-                //style={styles.field}
+                // style={styles.field}
                 label='Task Description:'
                 characterRestriction={100}
                 value={taskDsr}
                 onChangeText={(taskDsr) => this.setState({taskDsr})}/>
 
                 <Dropdown
-                label='Related BackLog'
-                data={data}
+                label= {this.state.fetchingBacklogs? 'Related BackLog : (fetching data ...)': 'Related BackLog :'}
+                disabled = {this.state.fetchingBacklogs}
+                //baseColor = {this.state.fetchingBacklogs? 'blue':'black'}
+                data={this.state.backlogData.map((item) => { return {value: item['name'],id: item['id']} })}
+                onChangeText={(value, index, data) => {this.setState({backlogId: data[index].id})}}
                 />
 
                 <View style={{paddingTop:20}}>
                 <Button 
                 title='Add Task'
-                onPress={() => console.log('Pressed')}
+                onPress={() => console.log(`taskName : ${this.state.taskName}\ntask dsr : ${this.state.taskDsr}\n backlogId : ${this.state.backlogId}`)}
                 type='solid'
                 />
                 </View>
-                
+            
                 </View>
             </View>
         );
