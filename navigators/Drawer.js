7dae06105d0ee9b678dd5312e13669443bb72e20
Mahdi Pazoki <mahdipazooki21@gmail.com>
2019-06-14 01:15:47 +0430

    make  as default screen

--- navigators/Drawer.js
+++ navigators/Drawer.js
@@ -24,7 +24,7 @@ const Drawer = createDrawerNavigator(
     }
   },
   {
-    initialRouteName: 'BacklogList',
+    initialRouteName: 'LoginScreen',
     drawerPosition: 'left',
     contentOptions: {
       activeTintColor: '#e91e63',
