fb8a2405e5fd67543a9553d08b33dbb700643a53
Mahdi Pazoki <mahdipazooki21@gmail.com>
2019-06-15 03:28:10 +0430

    add conditions of rendering login/normal process

--- App.js
+++ App.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AsyncStorage, ActivityIndicator, View } from 'react-native';
 import AppDrawer from './navigators/Drawer';
 import { createAppContainer } from 'react-navigation';
-import Task from './screens/Task'
-import AddTask from './screens/AddTask';
+import Login from './screens/LoginScreen'
 
 const AppContainer = createAppContainer(AppDrawer);
 
 
 export default class App extends React.Component {
+  state = {
+    loggedIn: "false",
+    isLoading: true
+  }
+  componentWillMount = async () => {
+    console.log('called')
+    const result = await AsyncStorage.getItem('loggedIn')
+    if (result == null || result === "false") this.setState({ loggedIn: "false" })
+    else this.setState({ loggedIn: "true" })
+    this.setState({ isLoading: false })
+  }
   render() {
+    const { isLoading, loggedIn } = this.state
     return (
-      <AppContainer />
+      isLoading ?
+        <View style={{ justifyContent: 'center', marginTop: "50%" }}>
+          <ActivityIndicator size="large" color="#DE94FF" />
+        </View>
+        :
+        loggedIn === "true" ? <AppContainer /> : <Login />
+
+
     );
   }
 }
