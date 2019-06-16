7fa3f5ef7776a5d91cf446e92505f71f3248005d
sadegh <mse737.khavari@gmail.com>
2019-06-16 21:45:12 +0430

    Modify task reducer function. Initiate a proper state for tasks in redux

--- reducers/tasks.js
+++ reducers/tasks.js
@@ -1,5 +1,28 @@
-const tasks = (state = [], action) => {
-    return state
+initalState = { //initialState for tasks data
+    to_do:[],
+    in_progress:[],
+    done:[]
+}
+const tasks = (state = initalState, action) => {
+    switch(action.type){
+        case("UPDATE_TODO"):
+            return {
+                ...state,
+                to_do: action.payload.data
+            }
+        case("UPDATE_INPROGRESS"):
+            return {
+                ...state,
+                in_progress: action.payload.data
+            }
+        case("UPDATE_DONE"):
+            return {
+                ...state,
+                done: action.payload.data
+            }
+        default:
+            return state
+    }
 }
 
 export default tasks
